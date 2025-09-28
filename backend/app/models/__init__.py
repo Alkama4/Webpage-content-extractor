0ee16cc3f1ab00d7aed8973b0782b599e5898734
Aleksi Malkki <aleksi.malkki@gmail.com>
2025-09-28 13:40:53 +0300

    Cleanup /models - Removed an accidental file. - Included new Pydantic models in `__init__.py`.

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,9 +1,9 @@
-from .webpage import WebpageBase, WebpageCreate, WebpageInDB, WebpagePatch
-from .scrape import ScrapeBase, ScrapeCreate, ScrapeInDB
+from .webpage import WebpageBase, WebpageCreate, WebpageInDB, WebpagePatch, WebpageOut
+from .scrape import ScrapeBase, ScrapeCreate, ScrapeInDB, ScrapePatch, ScrapeOut
 from .scrape_data import ScrapeData
 
 __all__ = [
-    "WebpageBase", "WebpageCreate", "WebpageInDB", "WebpagePatch",
-    "ScrapeBase", "ScrapeCreate", "ScrapeInDB",
+    "WebpageBase", "WebpageCreate", "WebpageInDB", "WebpagePatch", "WebpageOut",
+    "ScrapeBase", "ScrapeCreate", "ScrapeInDB", "ScrapePatch", "ScrapeOut",
     "ScrapeData",
 ]
