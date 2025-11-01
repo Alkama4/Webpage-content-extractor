ba69c65b52106ceb69a6b7a1297ed7ba163ff357
Aleksi Malkki <aleksi.malkki@gmail.com>
2025-11-01 23:58:32 +0200

    Fix create webpage form discrepencies - Users were not informed that the schedule defaults to 04:00 when left blank, and the field now defaults explicitly to 04:00 in addition to a warning message if blank. - The "Scraping enabled" toggle was ineffective. Pages always scraped by default.

--- frontend/src/components/FormWebpage.vue
+++ frontend/src/components/FormWebpage.vue
@@ -6,6 +6,12 @@
             @close="requestErrorMsg = ''"
             v-if="requestErrorMsg"
         />
+        <InlineMessage 
+            text="If the schedule is left blank, 04:00:00 will be used." 
+            :interaction="false"
+            type="warning"
+            v-if="!newWebpageDetails.run_time"
+        />
         <form @submit.prevent="webpageCreateOrUpdate">
             <TextInput
                 v-model="newWebpageDetails.page_name"
@@ -56,7 +62,9 @@ export default {
             selectedWebpage: null,
             newWebpageDetails: {
                 url: '',
-                page_name: ''
+                page_name: '',
+                run_time: '04:00:00',
+                is_enabled: false
             }
         }
     },
