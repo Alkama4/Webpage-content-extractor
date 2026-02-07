c590f617f879679862f40b2d23436544d759d47c
Aleksi Malkki <aleksi.malkki@gmail.com>
2026-02-07 17:34:25 +0200

    Setup pinia

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
 import '@/assets/global.css'
 import '@/assets/variables.css'
 import '@/assets/fonts.css'
 import '@/assets/utils.css'
 import 'boxicons/css/boxicons.min.css';
 
-const app = createApp(App)
+const app = createApp(App);
+const pinia = createPinia();
 
-app.use(router)
+app.use(pinia);
+app.use(router);
 
 app.mount('#app')
